f67422cd26b3f1f186935f67386cb5761a2bd394
espanderlof <jaime717@hotmail.es>
2024-04-13 16:43:49 -0400

    Inicio del proyecto

---
+++ src/main/java/com/jzs/ms_atenciones_medicas/MsAtencionesMedicasApplication.java
@@ -0,0 +1,13 @@
+package com.jzs.ms_atenciones_medicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAtencionesMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAtencionesMedicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jzs/ms_atenciones_medicas/MsAtencionesMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jzs.ms_atenciones_medicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAtencionesMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
